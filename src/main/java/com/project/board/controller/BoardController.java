1892d2f4879b5b557b3a2d4bf3218821bf918ae9
leedong617 <leedong617@gmail.com>
2024-07-17 13:36:33 +0900

    spring boot, mybatis, thymeleaf board practice 파일첨부 기능 구현

---
+++ src/main/java/com/project/board/controller/BoardController.java
@@ -0,0 +1,92 @@
+package com.project.board.controller;
+
+import com.project.board.dto.BoardDTO;
+import com.project.board.dto.BoardFileDTO;
+import com.project.board.dto.PageDTO;
+import com.project.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) throws IOException {
+        System.out.println("boardDTO = " + boardDTO);
+        boardService.save(boardDTO);
+        return "redirect:/paging";
+    }
+
+    @GetMapping("/list")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        System.out.println("boardDTOList = " + boardDTOList);
+        return "list";
+    }
+
+    //id로 게시글 조회
+    @GetMapping("/detail")
+    public String findById(@RequestParam("id") Long id,
+                           @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+                           Model model) {
+        // 조회수 처리
+        boardService.updateHits(id);
+        // 상세내용 가져옴
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        model.addAttribute("page", page);
+        System.out.println("boardDTO = " + boardDTO);
+        System.out.println("page = " + page);
+        if (boardDTO.getFileAttached() == 1) {
+            List<BoardFileDTO> boardFileDTOList = boardService.findFile(id);
+            model.addAttribute("boardFileList", boardFileDTOList);
+        }
+        return "detail";
+    }
+
+    @GetMapping("update/{id}")
+    public String update(@PathVariable("id") Long id, Model model) {
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(BoardDTO boardDTO, Model model) {
+        boardService.update(boardDTO);
+        BoardDTO dto = boardService.findById(boardDTO.getId());
+        model.addAttribute("board", dto);
+        return "detail";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        boardService.delete(id);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/paging")
+    public String paging(Model model,
+                         @RequestParam(value = "page", required = false, defaultValue = "1") int page) {
+        System.out.println("page = " + page);
+        List<BoardDTO> pagingList = boardService.pagingList(page);
+        PageDTO pageDTO = boardService.pagingParam(page);
+        model.addAttribute("boardList", pagingList);
+        model.addAttribute("paging", pageDTO);
+        return "paging";
+    }
+}
---
+++ src/main/java/com/project/board/repository/BoardRepository.java
@@ -0,0 +1,57 @@
+package com.project.board.repository;
+
+import com.project.board.dto.BoardDTO;
+import com.project.board.dto.BoardFileDTO;
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardRepository {
+    private final SqlSessionTemplate sql;
+
+    public BoardDTO save(BoardDTO boardDTO) {
+        sql.insert("Board.save", boardDTO);
+        return boardDTO;
+    }
+
+    public List<BoardDTO> findAll() {
+        return sql.selectList("Board.findAll");
+    }
+
+    public void updateHits(Long id) {
+        sql.update("Board.updateHits", id);
+    }
+
+    public BoardDTO findById(Long id) {
+        return sql.selectOne("Board.findById", id);
+    }
+
+    public void update(BoardDTO boardDTO) {
+        sql.update("Board.update", boardDTO);
+    }
+
+    public void delete(Long id) {
+        sql.delete("Board.delete", id);
+    }
+
+    public void saveFile(BoardFileDTO boardFileDTO) {
+        sql.insert("Board.saveFile", boardFileDTO);
+    }
+
+    public List<BoardFileDTO> findFile(Long id) {
+        return sql.selectList("Board.findFile", id);
+    }
+
+    public List<BoardDTO> pagingList(Map<String, Integer> pagingParams) {
+        return sql.selectList("Board.pagingList", pagingParams);
+    }
+
+    public int boardCount() {
+        return sql.selectOne("Board.boardCount");
+    }
+}
